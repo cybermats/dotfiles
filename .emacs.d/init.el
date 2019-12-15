bfcea8f607036019f7495e457c6ed6f087ad6f8a
Mats Fredriksson <cybermats@gmail.com>
2019-12-15 14:40:37 +0100

    Adding GO and ASM to emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,6 +23,7 @@ There are two things you can do about this warning:
 2. Remove this warning from your init file so you won't see it again."))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
@@ -90,6 +91,7 @@ There are two things you can do about this warning:
 
 ;; jsx
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
+(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
 
 (defun web-mode-init-hook ()
   "Hooks for Web mode. Adjust indent."
@@ -117,3 +119,90 @@ There are two things you can do about this warning:
 
 (add-hook 'web-mode-hook 'web-mode-init-prettier-hook)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-go gotest go-projectile go-autocomplete exec-path-from-shell yaml-mode web-narrow-mode web-mode-edit-element prettier-js json-mode groovy-mode go-mode flycheck-rtags flycheck-irony dockerfile-mode cpputils-cmake company-irony-c-headers company-irony cmake-ide auto-complete-clang add-node-modules-path))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$"
+			  ""
+			  (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+
+(defun my-go-mode-hook ()
+      (setq tab-width 2 indent-tabs-mode 1)
+      ; eldoc shows the signature of the function at point in the status bar.
+      (go-eldoc-setup)
+      
+      (local-set-key (kbd "M-.") #'godef-jump)
+      (add-hook 'before-save-hook 'gofmt-before-save)
+      (setq compile-command "echo Building... && go build -v && echo Testing... && go test -v && echo Linter... && golint")
+      (setq compilation-read-command nil)
+      ; extra keybindings from https://github.com/bbatsov/prelude/blob/master/modules/prelude-go.el
+      (let ((map go-mode-map))
+        (define-key map (kbd "C-c a") 'go-test-current-project) ;; current package, really
+        (define-key map (kbd "C-c m") 'go-test-current-file)
+        (define-key map (kbd "C-c .") 'go-test-current-test)
+        (define-key map (kbd "C-c b") 'go-run)
+	(local-set-key (kbd "M-,") 'compile)
+	))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+; "projectile" recognizes git repos (etc) as "projects" and changes settings
+; as you switch between them. 
+(projectile-global-mode 1)
+(require 'go-projectile)
+(go-projectile-tools-add-path)
+(setq gofmt-command (concat go-projectile-tools-path "/bin/goimports"))
+
+
+; "company" is auto-completion
+(require 'company)
+(require 'go-mode)
+(require 'company-go)
+(add-hook 'go-mode-hook (lambda ()
+                          (company-mode)
+                          (set (make-local-variable 'company-backends) '(company-go))))
+
+;;Smaller compilation buffer
+(setq compilation-window-height 14)
+(defun my-compilation-hook ()
+  (when (not (get-buffer-window "*compilation*"))
+    (save-selected-window
+      (save-excursion
+        (let* ((w (split-window-vertically))
+               (h (window-height w)))
+          (select-window w)
+          (switch-to-buffer "*compilation*")
+          (shrink-window (- h compilation-window-height)))))))
+(add-hook 'compilation-mode-hook 'my-compilation-hook)
+
+;;Other Key bindings
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
+;;Compilation autoscroll
+(setq compilation-scroll-output t)
+
+
+(defun my-asm-mode-hook ()
+  ;; you can use `comment-dwim` (M-;) for this kind of behaviour anyway
+  (local-unset-key (vector asm-comment-char))
+  ;; asm-mode sets it locally to nil, to "stay closer to the old TAB behaviour".
+  (setq tab-always-indent (default-value 'tab-always-indent)))
+(add-hook 'asm-mode-hook #'my-asm-mode-hook)
