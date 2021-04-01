8ed1bfc60a6f73030df067cae9568de4723e045c
Mats Fredriksson <cybermats@gmail.com>
2021-04-01 12:08:11 +0200

    Cleaning up init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,17 +4,50 @@
 
 ;;; Commentary:
 
-
+;;; Code:
 (tool-bar-mode -1)
 (setq inhibit-startup-screen t)
 
 
+;;; Configure WindMove
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Programming stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable company-mode for all buffers
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Enable lsp-mode
+(require 'lsp)
+(add-hook 'c-mode-hook #'lsp)
+
+;; Configure ccls
+(require 'ccls)
+(setq ccls-executable "/usr/bin/ccls")
+
+;; Configure Flymake
+(require 'flymake)
+(define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
+(define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error)
+
+;; Set up find other file (for .h to .c)
+(global-set-key (kbd "C-c o") 'ff-find-other-file)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Other
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;; melpa package manager
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
+		    (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
   (when no-ssl
     (warn "\
@@ -25,7 +58,6 @@ There are two things you can do about this warning:
 2. Remove this warning from your init file so you won't see it again."))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
@@ -33,109 +65,22 @@ There are two things you can do about this warning:
 (package-initialize)
 
 
-;; Irony source code completion
-(require 'cc-mode)
-(require 'irony)
-
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-;; Company mode
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-
-(require 'company-irony-c-headers)
-(with-eval-after-load 'company
-  (setq company-backends (delete 'company-semantic company-backends))
-  '(add-to-list
-    'company-backends '(company-irony-c-headers company-irony)))
-
-(setq company-idle-delay 0)
-(define-key c-mode-map [(tab)] 'company-complete)
-(define-key c++-mode-map [(tab)] 'company-complete)
-
-(add-hook 'c++-mode-hook 'company-mode)
-(add-hook 'c-mode-hook 'company-mode)
-
-;; Flycheck
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'flycheck-mode)
-
-(require 'flycheck-rtags)
-
-(defun my-flycheck-rtags-setup ()
-  (flycheck-select-checker 'rtags)
-  (setq-local flycheck-highlighting-mod nil) ;; RTags creates more accurate overlays
-  (setq-local flycheck-check-syntax-automatically nil))
-;; c-mode-common-hook is also called by c++-mode
-(add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
-
-;; Flycheck - irony integration
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-
-;; CMake ide
-(cmake-ide-setup)
-
-(add-hook 'go-mode-hook
-	  (lambda ()
-	    (setq tab-width 4)
-	    (setq indent-tabs-mode 1)))
-
-;; jsx
-(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
-
-(defun web-mode-init-hook ()
-  "Hooks for Web mode. Adjust indent."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-attr-indent-offset 2)
-)
-
-(add-hook 'web-mode-hook 'web-mode-init-hook)
-
-(setq-default flycheck-disabled-checkers
-	      (append flycheck-disabled-checkers
-		      '(javascript-jshint json-jsonlist emacs-list-checkdoc)))
-
-
-; Handle SCHEME support through guile
+					; Handle SCHEME support through guile
 (setq geiser-active-implementations '(chicken))
 
-;; Enable eslint checker for web-mode
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-;; Enable flycheck globally
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-(add-hook 'flycheck-mode-hook 'add-node-modules-path)
-
-(defun web-mode-init-prettier-hook ()
-  (add-node-modules-path)
-  (prettier-js-mode))
-
-(add-hook 'web-mode-hook 'web-mode-init-prettier-hook)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes '(tango-dark))
  '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(package-selected-packages
-   (quote
-    (geiser elpy spacemacs-theme mwim ewal company-go gotest go-projectile go-autocomplete exec-path-from-shell yaml-mode web-narrow-mode web-mode-edit-element prettier-js json-mode groovy-mode go-mode flycheck-rtags flycheck-irony dockerfile-mode cpputils-cmake company-irony-c-headers company-irony cmake-ide auto-complete-clang add-node-modules-path))))
+   '(ccls lsp-mode company project markdown-mode geiser mwim ewal)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -143,93 +88,13 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  )
 
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-			  "[ \t\n]*$"
-			  ""
-			  (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell users
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(when window-system (set-exec-path-from-shell-PATH))
-
-
-(defun my-go-mode-hook ()
-      (setq tab-width 2 indent-tabs-mode 1)
-      ; eldoc shows the signature of the function at point in the status bar.
-      (go-eldoc-setup)
-      
-      (local-set-key (kbd "M-.") #'godef-jump)
-      (add-hook 'before-save-hook 'gofmt-before-save)
-      (setq compile-command "echo Building... && go build -v && echo Testing... && go test -v && echo Linter... && golint")
-      (setq compilation-read-command nil)
-      ; extra keybindings from https://github.com/bbatsov/prelude/blob/master/modules/prelude-go.el
-      (let ((map go-mode-map))
-        (define-key map (kbd "C-c a") 'go-test-current-project) ;; current package, really
-        (define-key map (kbd "C-c m") 'go-test-current-file)
-        (define-key map (kbd "C-c .") 'go-test-current-test)
-        (define-key map (kbd "C-c b") 'go-run)
-	(local-set-key (kbd "M-,") 'compile)
-	))
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-
-; "projectile" recognizes git repos (etc) as "projects" and changes settings
-; as you switch between them. 
-(projectile-global-mode 1)
-(require 'go-projectile)
-(go-projectile-tools-add-path)
-(setq gofmt-command (concat go-projectile-tools-path "/bin/goimports"))
-
-
-; "company" is auto-completion
-(require 'company)
-(require 'go-mode)
-(require 'company-go)
-(add-hook 'go-mode-hook (lambda ()
-                          (company-mode)
-                          (set (make-local-variable 'company-backends) '(company-go))))
-
-;;Smaller compilation buffer
-(setq compilation-window-height 14)
-(defun my-compilation-hook ()
-  (when (not (get-buffer-window "*compilation*"))
-    (save-selected-window
-      (save-excursion
-        (let* ((w (split-window-vertically))
-               (h (window-height w)))
-          (select-window w)
-          (switch-to-buffer "*compilation*")
-          (shrink-window (- h compilation-window-height)))))))
-(add-hook 'compilation-mode-hook 'my-compilation-hook)
-
-;;Other Key bindings
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
 ;;Compilation autoscroll
 (setq compilation-scroll-output t)
-
-
-(defun my-asm-mode-hook ()
-  ;; you can use `comment-dwim` (M-;) for this kind of behaviour anyway
-  (local-unset-key (vector asm-comment-char))
-  ;; asm-mode sets it locally to nil, to "stay closer to the old TAB behaviour".
-  (setq tab-always-indent (default-value 'tab-always-indent)))
-(add-hook 'asm-mode-hook #'my-asm-mode-hook)
 
 (setq ewal-use-built-in-on-failure-p t)
 
 ;; Add my personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/lisp")
-
-;; Load gas-mode
-;(load "gas-mode")
-
-
-(load "z80-mode")
-(add-to-list 'auto-mode-alist '("\\.s\\'" . z80-mode))
-
-;; Enable elpy
-(elpy-enable)
 
 
 (provide 'init)
